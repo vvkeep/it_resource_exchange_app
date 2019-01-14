4cb558a24d70aec03a71b62c30367cd5bc10d7e2
姚巍 <15919688564@163.com>
2019-01-14 21:40:03 +0800

    add login file

--- lib/pages/profile/profile_header_info.dart
+++ lib/pages/profile/profile_header_info.dart
@@ -7,46 +7,51 @@ class ProfileHeaderInfoView extends StatelessWidget {
     return Container(
       height: 80,
       color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(width: 20),
-          Expanded(
-            child: GestureDetector(
-              onTap: () {
-                print("点击了头部登录");
-              },
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Text('请先登录', style: Theme.of(context).textTheme.title.copyWith(fontSize: 20),),
-                  Padding(
-                    padding: EdgeInsets.only(top: 6.0),
-                    child: Text(
-                      '此人很懒，什么都没写~',
-                      style: Theme.of(context).textTheme.body1.copyWith(fontSize: 12, color: Color(0xff818181)),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: ClipOval(
-              child: Icon(
-                IconData(
-                  0xe642,
-                  fontFamily: Constant.IconFontFamily,
+      child: FlatButton(
+        onPressed: () {
+          print("点击头部信息");
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(width: 20),
+            Expanded(
+              child: GestureDetector(
+                onTap: () {
+                  print("点击了头部登录");
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text('请先登录', style: Theme.of(context).textTheme.title.copyWith(fontSize: 20),),
+                    Padding(
+                      padding: EdgeInsets.only(top: 6.0),
+                      child: Text(
+                        '此人很懒，什么都没写~',
+                        style: Theme.of(context).textTheme.body1.copyWith(fontSize: 12, color: Color(0xff818181)),
+                      ),
+                    )
+                  ],
                 ),
-                size: 60.0, 
-                color: Color(AppColors.ArrowNormalColor),
               ),
             ),
-          )
-        ],
-      ),
+            Padding(
+              padding: EdgeInsets.only(right: 20),
+              child: ClipOval(
+                child: Icon(
+                  IconData(
+                    0xe642,
+                    fontFamily: Constant.IconFontFamily,
+                  ),
+                  size: 60.0, 
+                  color: Color(AppColors.ArrowNormalColor),
+                ),
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
