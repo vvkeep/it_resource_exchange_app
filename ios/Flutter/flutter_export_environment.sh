d3c94f466dda714cce0a66b90d78ad84ff470896
15919688564@163.com <15919688564@163.com>
2019-10-14 23:48:14 +0800

    new version 1.0.2

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,10 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/yaowei/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/yaowei/Documents/GitPro/it_resource_exchange_app"
-export "FLUTTER_TARGET=/Users/yaowei/Documents/GitPro/it_resource_exchange_app/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/yaowei/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/yaowei/Documents/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.1"
-export "FLUTTER_BUILD_NUMBER=7"
-export "TRACK_WIDGET_CREATION=true"
+export "FLUTTER_BUILD_NUMBER=0"
